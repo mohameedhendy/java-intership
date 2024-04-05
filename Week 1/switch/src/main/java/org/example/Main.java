cff714e5624d2f9c702a3036f902597692d9d017
mohameedhendy <138932041+mohameedhendy@users.noreply.github.com>
2024-04-05 14:37:03 +0200

    Add files via upload

---
+++ Week 1/switch/src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter Weekday Number : ");
+        int weekDayNum = s.nextInt();
+
+        if(weekDayNum == 1){
+            System.out.println("Monday");
+        } else if(weekDayNum == 2){
+            System.out.println("Tuesday");
+        } else if(weekDayNum == 3){
+            System.out.println("wednesday");
+        } else if(weekDayNum == 4){
+            System.out.println("thursday");
+        } else if(weekDayNum == 5){
+            System.out.println("Friday");
+        } else if(weekDayNum == 6){
+            System.out.println("Saturday");
+        } else if(weekDayNum == 7){
+            System.out.println("Sunday");
+        } else {
+            System.out.println("Invalid number.Please, enter a number between 1 : 7");
+        }
+
+    }
+}
